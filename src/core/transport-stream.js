5ac8ab1e0d00b42e148e000c213329de903e20ce
Vinícius Lourenço <contact@viniciusl.com.br>
2024-05-29 23:16:23 -0300

    remove deps and unused code from transport stream

--- src/core/transport-stream.js
+++ src/core/transport-stream.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const { realImport, realRequire } = require('real-require')
-
 module.exports = loadTransportStreamBuilder
 
 /**
@@ -10,32 +8,19 @@ module.exports = loadTransportStreamBuilder
  * @returns {function(object): Promise<import('stream').Writable>}
  * @throws {Error} In case the target module does not export a function
  */
-async function loadTransportStreamBuilder (target) {
+async function loadTransportStreamBuilder(target) {
   let fn
   try {
     const toLoad = target.startsWith('file://') ? target : 'file://' + target
 
-    if (toLoad.endsWith('.ts') || toLoad.endsWith('.cts')) {
-      // TODO: add support for the TSM modules loader ( https://github.com/lukeed/tsm ).
-      if (process[Symbol.for('ts-node.register.instance')]) {
-        realRequire('ts-node/register')
-      } else if (process.env && process.env.TS_NODE_DEV) {
-        realRequire('ts-node-dev')
-      }
-      // TODO: Support ES imports once tsc, tap & ts-node provide better compatibility guarantees.
-      fn = realRequire(decodeURIComponent(target))
-    } else {
-      fn = (await realImport(toLoad))
-    }
+    fn = (await import(toLoad))
   } catch (error) {
     // See this PR for details: https://github.com/pinojs/thread-stream/pull/34
     if ((error.code === 'ENOTDIR' || error.code === 'ERR_MODULE_NOT_FOUND')) {
-      fn = realRequire(target)
-    } else if (error.code === undefined || error.code === 'ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING') {
+      fn = require(target)
+    } else if (error.code === undefined) {
       // When bundled with pkg, an undefined error is thrown when called with realImport
-      // When bundled with pkg and using node v20, an ERR_VM_DYNAMIC_IMPORT_CALLBACK_MISSING error is thrown when called with realImport
-      // More info at: https://github.com/pinojs/thread-stream/issues/143
-      fn = realRequire(decodeURIComponent(target))
+      fn = require(decodeURIComponent(target))
     } else {
       throw error
     }
